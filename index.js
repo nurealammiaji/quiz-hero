90792be6f915b7b615ae4b9957b326ea1bb0eca3
nurealammiaji <nurealam.miaji@gmail.com>
2023-06-26 13:03:58 +0600

    event listener added to the blog button

--- index.js
+++ index.js
@@ -183,3 +183,8 @@ document.querySelector("#submit").addEventListener("click", () => {
   }, 1500);
   window.scrollTo(0, 0);
 });
+
+// Blog Code
+document.querySelector("#blog-btn").addEventListener("click", () => {
+  console.log("blog button clicked");
+})
